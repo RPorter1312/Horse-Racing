14cc7295c66eee27eeb0024ef35a767237a751c2
Ryan Porter <rporter1312@gmail.com>
2025-11-16 16:59:26 +0000

    Removed entrypoint from dbutils, which is now in 'upload.py'

--- racingmodel/db/dbutils.py
+++ racingmodel/db/dbutils.py
@@ -1,21 +1,7 @@
 import sqlalchemy
 import pandas as pd
-import logging
 import os
-import argparse
-from pathlib import Path
 from typing import Literal
-
-logging.getLogger(__name__)
-
-env_vars = {
-    "user": os.environ.get("hruser"),
-    "pass": os.environ.get("hrpass"),
-    "db_name": os.environ.get("hrdatabase"),
-}
-engine = sqlalchemy.create_engine(
-    f"postgresql+psycopg2://{env_vars['user']}:{env_vars['pass']}@localhost/{env_vars['db_name']}"
-)
 
 
 def upload_csv_to_pg(engine: sqlalchemy.Engine, csv_file_path: str | os.PathLike, sql: str):
@@ -40,26 +26,3 @@ def upload_df_to_pg(
 def import_pg_to_df(engine: sqlalchemy.engine, table_name: str) -> pd.DataFrame:
     with engine.connection() as conn:
         return pd.read_sql_table(table_name=table_name, con=conn)
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--csv_file", type=str, required=True)
-    args = parser.parse_args()
-
-    root_dir = Path(__file__).parents[2]
-    racing_hist_imp_sql_path = Path(
-        root_dir,
-        'racingmodel',
-        'db',
-        'sql', 
-        'racing_history_csv_import.sql'
-    )
-
-    with open(racing_hist_imp_sql_path) as f:
-        sql = f.read()
-        upload_csv_to_pg(
-            engine,
-            csv_file_path=args.csv_file,
-            sql=sql
-        )
