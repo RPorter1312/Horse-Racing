fd6f57b43e8fd8bca6d3f6610f442b4d29c96fb8
Ryan Porter <rporter1312@gmail.com>
2025-12-30 13:39:42 +0000

    Reorganised parsing file

--- racingmodel/utils/parsing.py
+++ racingmodel/utils/parsing.py
@@ -4,40 +4,6 @@ import os
 
 from .processing import dist_to_furlongs
 
-
-def parse_racecards(json_file: str | os.PathLike, regions: list[str]) -> pd.DataFrame:
-    with open(json_file) as f:
-        racecards = json.load(f)
-
-    races = []
-
-    for region in regions:
-        for course in racecards[region].keys():
-            for off_time in racecards[region][course].keys():
-                race = pd.json_normalize(
-                    data=racecards[region][course][off_time],
-                    record_path="runners",
-                    meta=[
-                        "date",
-                        "course",
-                        "race_id",
-                        "off_time",
-                        "race_name",
-                        "distance",
-                        "going",
-                        "pattern",
-                        "type",
-                        "race_class",
-                        "age_band",
-                        "rating_band",
-                        "prize",
-                    ],
-                )
-                races.append(race)
-
-    return pd.concat(races)
-
-
 def prep_racecard_data(data: pd.DataFrame) -> pd.DataFrame:
     data = data.copy()
 
@@ -91,3 +57,37 @@ def prep_racecard_data(data: pd.DataFrame) -> pd.DataFrame:
     data_renamed["dist_f"] = data_renamed["dist"].apply(lambda x: dist_to_furlongs(x))
 
     return data_renamed
+
+def parse_racecards(json_file: str | os.PathLike, regions: list[str]) -> pd.DataFrame:
+    with open(json_file) as f:
+        racecards = json.load(f)
+
+    races = []
+
+    for region in regions:
+        for course in racecards[region].keys():
+            for off_time in racecards[region][course].keys():
+                race = pd.json_normalize(
+                    data=racecards[region][course][off_time],
+                    record_path="runners",
+                    meta=[
+                        "date",
+                        "course",
+                        "race_id",
+                        "off_time",
+                        "race_name",
+                        "distance",
+                        "going",
+                        "pattern",
+                        "type",
+                        "race_class",
+                        "age_band",
+                        "rating_band",
+                        "prize",
+                    ],
+                )
+                races.append(race)
+
+    racecard_data = pd.concat(races)
+
+    return prep_racecard_data(racecard_data)
